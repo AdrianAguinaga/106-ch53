d3a4fbe18ae3b3c505e32a79bbd1234f5b28ea43
nefersillo <adrian.aguinaga@uabc.edu.mx>
2024-12-09 19:22:56 -0800

    session4

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,21 @@ function saveTask()
     console.log(taskToSave);
 
     //save to server
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(response) {
+        console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 
     //display the task
-    displayTask(taskToSave);
+    //displayTask(taskToSave);
 }
 
 function displayTask(task)
@@ -37,7 +49,7 @@ function displayTask(task)
 function testRequest(){
     $.ajax({
         type: "get",
-        url: "http://fsdiapi.azurewebsites.net", 
+        url: "http://fsdiapi.azurewebsites.net/", 
         success: function(response){
             console.log(response);
         },
@@ -49,9 +61,36 @@ function testRequest(){
     });
 }
 
+function loadTask(){
+$.ajax({
+    type: "get",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks",
+
+    success:function(response){
+        console.log(response);
+        let data = JSON.parse(response);
+        for(let i= 0; i<data.length;i++){
+            let task = data[i];
+            if (task.name === "Adrian53"){
+                displayTask(task);
+            }
+        }
+        console.log(data);
+    },
+    error:function(error){
+        console.log(error);
+    }    
+})
+}
+// from the object returned, get only messages that were created by you.
+// tip 1. Modify the class to include your user.
+// tip 2. use a for loop
+// tip 3. Then an if statement
+
 function init() {
     console.log("task manager");
     //load data
+    loadTask();
 
     //hook the events
     $("#btnSave").click(saveTask);
